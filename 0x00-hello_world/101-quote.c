17192d67e840c925e44e3f01f88bbdc47e261932
KHALgendy <khaledelgendy464@gmail.com>
2023-03-17 08:21:36 -0700

    77

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 
-int main(void) {
-  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDERR_FILENO, message, sizeof(message)-1);
-  return 1;
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
 
